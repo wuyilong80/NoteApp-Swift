a85970efd195b41edc6a563071b82a04e794045f
wuyilong <wuyilong80@gmail.com>
2017-07-31 23:33:21 +0800

    0731 draw image still fail

--- NoteApp/NoteApp/Note.swift
+++ NoteApp/NoteApp/Note.swift
@@ -42,4 +42,68 @@ class Note:NSManagedObject{
         }
         return nil
     }
+    
+//    func drawImage() -> UIImage? {
+//        
+//        var smallImage = self.image()
+//        if (image() != nil) {
+//            return nil
+//        }
+////        print("12341234\(smallImage)")
+//        let thumbnailSize = CGSize(width: 36, height: 36)
+//        let scale = UIScreen.main.scale
+//        UIGraphicsBeginImageContextWithOptions(thumbnailSize, false, scale)
+//        
+//        let widthRatio = thumbnailSize.width/(smallImage?.size.width)!
+//        let heightRatio = thumbnailSize.height/(smallImage?.size.height)!
+//        
+//        UIGraphicsBeginImageContext(CGSize(width:widthRatio, height:heightRatio))
+//        smallImage?.draw(in: CGRect(x:0, y:0, width:widthRatio , height:heightRatio))
+//        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//        
+//        return smallImage
+//    }
+
+    func imageWithImage (sourceImage:UIImage, scaledToWidth: CGFloat) -> UIImage {
+        
+        let oldWidth = sourceImage.size.width
+        let scaleFactor = scaledToWidth / oldWidth
+        
+        let newHeight = sourceImage.size.height * scaleFactor
+        let newWidth = oldWidth * scaleFactor
+        
+        UIGraphicsBeginImageContext(CGSize(width:newWidth, height:newHeight))
+        sourceImage.draw(in: CGRect(x:0, y:0, width:newWidth, height:newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
 }
+
+//CGSize thumbnailSize = CGSizeMake(36, 36); //設定縮圖大小
+//CGFloat scale = [UIScreen mainScreen].scale; //找出目前螢幕的scale，是網膜技術為2.0
+////產生畫布，第一個參數指定大小，第二個參數YES:不透明(黑色底) NO表示背景透明，scale為螢幕scale
+//UIGraphicsBeginImageContextWithOptions(thumbnailSize, NO, scale);
+//
+////MAX 結果會等於 UIViewContentModeScaleAspectFill
+////MIN 結果會等於 UIViewContentModeScaleAspectFit
+//CGFloat widthRatio = thumbnailSize.width / image.size.width;
+//CGFloat heightRadio = thumbnailSize.height / image.size.height;
+//CGFloat ratio = MAX(widthRatio,heightRadio);
+//
+//CGSize imageSize = CGSizeMake(image.size.width*ratio, image.size.height*ratio);
+//
+////切成圓形
+//UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, thumbnailSize.width, thumbnailSize.height)];
+//[circlePath addClip];
+
+//[image drawInRect:CGRectMake(-(imageSize.width-thumbnailSize.width)/2.0,
+//    -(imageSize.height-thumbnailSize.height)/2.0,
+//    imageSize.width, imageSize.height)];
+//
+////取得畫布上的縮圖
+//image = UIGraphicsGetImageFromCurrentImageContext();
+////關掉畫布
+//UIGraphicsEndImageContext();
+//return image;
